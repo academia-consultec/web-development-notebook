f0a4cb30c77a4445073da45a3312c629933421f2
Alexander Diaz <hello@alexanderdiaz.net>
2023-02-27 19:30:20 -0500

    FEAT: JavaScript interactions

--- src/js/notebook.js
+++ src/js/notebook.js
@@ -1,3 +1,22 @@
+var greetingMessage = "Saludos desde el archivo .js";
+
 function sayHello() {
-    alert("Saludos desde el archivo .js");
+    alert(greetingMessage);
 }
+
+// solventa el error de carga cuando el DOM no esta listo
+window.addEventListener('DOMContentLoaded', () => {
+
+    const principalHeader = document.getElementById('header');
+
+    // indicamos que cambie el color cuando el mouse esta sobre el item
+    principalHeader.addEventListener('mouseover', function handleMouseOver() {
+        principalHeader.style.color = 'red';
+    });
+
+    // indicamos que recupere el color cuando el mouse no esta sobre el item
+    principalHeader.addEventListener('mouseout', function handleMouseOut() {
+        principalHeader.style.color = '#226666';
+    });
+
+});
