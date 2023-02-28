129c3c1db24cb892bb64315515dc96c09251a40e
Alexander Diaz <hello@alexanderdiaz.net>
2023-02-27 20:04:25 -0500

    CHORE: Best Practices

--- src/js/notebook.js
+++ src/js/notebook.js
@@ -1,29 +1,33 @@
-var greetingMessage = "Saludos desde el archivo .js";
+const greetingMessage = "Saludos desde el archivo .js";
 
 function sayHello() {
     alert(greetingMessage);
 }
 
+// Colors
+const principal = "#226666";
+const tertiary =  "#D46A6A";
+
 // solventa el error de carga cuando el DOM no esta listo
 window.addEventListener('DOMContentLoaded', () => {
 
-    const principalItem = document.getElementById('header');
-    const secundaryItem = document.getElementById('image');
+    const headerElement = document.getElementById('header');
+    const imageElement = document.getElementById('image');
 
     const eventHandler = (targetItem, color = 'red') => {
         targetItem.style.color = color;
     }
 
     // indicamos que cambie el color cuando el mouse esta sobre el item
-    principalItem.addEventListener('mouseover', () => eventHandler(principalItem));
+    headerElement.addEventListener('mouseover', () => eventHandler(headerElement));
 
     // indicamos que recupere el color cuando el mouse no esta sobre el item
-    principalItem.addEventListener('mouseout', () => eventHandler(principalItem, '#226666'));
+    headerElement.addEventListener('mouseout', () => eventHandler(headerElement, principal));
 
     // indicamos que cambie el color cuando el mouse esta sobre el item
-    secundaryItem.addEventListener('mouseover', () => eventHandler(principalItem, '#D46A6A'));
+    imageElement.addEventListener('mouseover', () => eventHandler(headerElement, tertiary));
 
     // indicamos que recupere el color cuando el mouse no esta sobre el item
-    secundaryItem.addEventListener('mouseout', () => eventHandler(principalItem, '#226666'));
+    imageElement.addEventListener('mouseout', () => eventHandler(headerElement, principal));
 
 });
